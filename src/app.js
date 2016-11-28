bc393bc8835c56276aa26295e8c5f10263ba00f4
Joshua Smith <joshua.aaron.smith17@gmail.com>
2016-11-28 11:48:03 -0500

    added toggle functionality

--- src/app.js
+++ src/app.js
@@ -1,36 +1,100 @@
 const React = require('react')
 
-// const { map } = require('fun-fp')
+const {map} = require('fun-fp')
 const TodoItem = require('./todo-item')
 
 const App = React.createClass({
-  render() {
-    return (
-      <section className="todoapp">
-        <header className="header">
-          <h1>Todos</h1>
-          <input className="new-todo" type="text" placeholder="What needs to be done?" autoFocus />
-        </header>
-        <section id="main">
-          <input type="checkbox" className="toggle-all" />
-          <label htmlFor="toggle-all">Mark all as complete</label>
-          <ul className="todo-list">
-          </ul>
-        </section>
-        <footer className="footer">
-          <span className="todo-count"><strong>0</strong> item(s) left</span>
-          <ul className="filters">
-            <li><a href="/">All</a></li>
-            <li>
-              <a href="/active">Active</a>
-            </li>
-            <li><a href="/completed">Completed</a></li>
-          </ul>
-          <button className="clear-completed">Clear completed</button>
-        </footer>
-      </section>
-    )
-  }
+    getInitialState() {
+        return {
+            newTodo: {
+                title: '',
+                completed: false
+            },
+            todos: [
+                {
+                    id: 1,
+                    title: 'Pick Final Project',
+                    completed: false
+                }, {
+                    id: 2,
+                    title: 'Plan Final Project',
+                    completed: false
+                }
+            ]
+        }
+    },
+    handleChange(e) {
+        this.setState({
+            newTodo: {
+                title: e.target.value
+            }
+        })
+    },
+    handleSubmit(e) {
+        e.preventDefault()
+        const newTodo = {
+            ...this.state.newTodo
+        }
+        newTodo.id = new Date().toISOString()
+        const todos = [
+            ...this.state.todos,
+            newTodo
+        ]
+        this.setState({
+            todos,
+            newTodo: {
+                title: '',
+                completed: false
+            }
+        })
+    },
+    handleToggle(todo) {
+        return (e) => {
+            const todos = map(td => {
+                if (td.id === todo.id) {
+                    td.completed = !td.completed
+                }
+                return td
+            }, [...this.state.todos])
+            this.setState({todos})
+        }
+    },
+    render() {
+        return (
+            <section className="todoapp">
+                <header className="header">
+                    <h1>Todos</h1>
+                    <form onSubmit={this.handleSubmit}>
+                        <input className="new-todo" type="text" placeholder="What needs to be done?" autoFocus onChange={this.handleChange} value={this.state.newTodo.title}/>
+                    </form>
+                </header>
+                <section id="main">
+                    <input type="checkbox" className="toggle-all"/>
+                    <label htmlFor="toggle-all">Mark all as complete</label>
+                    <ul className="todo-list">
+                        {map(todo => <TodoItem key={todo.id} todo={todo} onToggle={this.handleToggle(todo)}/>, this.state.todos)}
+                    </ul>
+                </section>
+                <footer className="footer">
+                    <span className="todo-count">
+                        <strong>0</strong>
+                        item(s) left</span>
+                    <ul className="filters">
+                        <li>
+                            <a href="/">All</a>
+                        </li>
+                        <li>
+                            <a href="/active">Active</a>
+                        </li>
+                        <li>
+                            <a href="/completed">Completed</a>
+                        </li>
+                    </ul>
+                    <button className="clear-completed">Clear completed</button>
+                </footer>
+            </section>
+        )
+    }
 })
 
 module.exports = App
--- src/todo-item.js
+++ src/todo-item.js
@@ -1,18 +1,20 @@
 const React = require('react')
 
 const TodoItem = React.createClass({
-  render () {
-    return (
-      <li>
-        <div className="view">
-          <input type="checkbox" className="toggle"/>
-          <label htmlFor="">{this.props.todo.title}</label>
-          <button className="destroy"></button>
-        </div>
-        <input type="text" className="edit"/>
-      </li>
-    )
-  }
+    render() {
+        return (
+            <li className={this.props.todo.completed
+                ? 'completed'
+                : ''}>
+                <div className="view">
+                    <input type="checkbox" className="toggle" checked={this.props.todo.completed} onChange={this.props.onToggle}/>
+                    <label htmlFor="">{this.props.todo.title}</label>
+                    <button className="destroy"></button>
+                </div>
+                <input type="text" className="edit"/>
+            </li>
+        )
+    }
 })
 
 module.exports = TodoItem
